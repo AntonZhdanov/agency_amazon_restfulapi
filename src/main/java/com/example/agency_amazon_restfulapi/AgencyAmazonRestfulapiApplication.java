ca5966c7ebe15c230890f51cb5facd0d261b70ae
Zhdanov Anton <anton1992xxxx@gmail.com>
2024-03-01 11:12:43 +0200

    Initial commit

---
+++ src/main/java/com/example/agency_amazon_restfulapi/AgencyAmazonRestfulapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.agency_amazon_restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgencyAmazonRestfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgencyAmazonRestfulapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agency_amazon_restfulapi/AgencyAmazonRestfulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agency_amazon_restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyAmazonRestfulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
